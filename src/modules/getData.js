74a14e626ff1242c253037827571fe95a1d6a5fe
iamidlek <iamidlek@naver.com>
2021-10-29 00:49:04 +0900

    refactor module getData

---
+++ src/modules/getData.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+// params 검색어, 페이지 정보를 받아 data 요청
+// return 전체 영화 수, 10개의 영화 정보(1페이지에 해당)
+export async function getData (keyword, page) {
+  const info = { keyword, page }
+  const { data } = await axios.post('/.netlify/functions/list', info)
+  return { maxPage: data.totalResults, list: data.Search }
+}
