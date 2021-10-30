5996f1fdff4d9e141ba7b4b4e26a7ca590c14609
iamidlek <iamidlek@naver.com>
2021-10-31 01:42:43 +0900

    뼈대 작업

---
+++ src/components/ToDoView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrapper">
+    <ToDoList class="todolist" />
+    <!-- 여기는 Done으로 전환할 버튼 추가 공간입니다 -->
+  </div>
+</template>
+
+<script>
+import ToDoList from '~/components/ToDoList'
+
+export default {
+  components: {
+    ToDoList
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .todolist {
+    transition: 1s ease-in-out;
+    // &:hover{
+    //   transform: rotateY(180deg);
+    //   backface-visibility: hidden;
+    // }
+  }
+}
+</style>
