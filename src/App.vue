ccbd4cab05d39e6114713d7817966eb825f06bec
iamidlek <iamidlek@naver.com>
2021-10-30 23:43:10 +0900

    login feat

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,62 @@
 <template>
-  <BackGroundStar />
-  <CheckName />
+  <BackGround />
+  <transition-group name="fade">
+    <CheckName
+      v-if="!admission"
+      v-model="open"
+      @enroll="signIn" />
+    <ToDoView v-if="open" />
+  </transition-group>
 </template> 
 
 <script>
-import BackGroundStar from '~/components/BackGroundStar'
+import BackGround from '~/components/BackGround'
 import CheckName from '~/components/CheckName'
+import ToDoView from '~/components/ToDoView'
 
 export default {
   components: {
-    BackGroundStar,
-    CheckName
+    BackGround,
+    CheckName,
+    ToDoView
+  },
+  data() {
+    return {
+      userName: null,
+      admission: true,
+      open: false
+    }
+  },
+  mounted() {
+    setTimeout(()=>{this.admission = false},100)
+    setTimeout(()=>{this.checkStorage()},2000)
+  },
+  methods: {
+    checkStorage() {
+      const user = localStorage.getItem('todo') 
+      if (user) {
+        this.admission = true
+        this.userName = user
+      }
+    },
+    signIn(res) {
+      this.admission = res
+      this.userName = localStorage.getItem('todo')
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.fade-enter-active{
+  opacity: 1;
+  transition: opacity 2s linear;
+}
+.fade-leave-active {
+  opacity: 0;
+  transition: opacity 1s linear;
+}
+.fade-enter-from{
+  opacity: 0;
+}
+</style>
