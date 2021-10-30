a243a40a087c064daab89d10370a3aba650ea7b7
iamidlek <iamidlek@naver.com>
2021-10-31 01:42:01 +0900

    born listitem

---
+++ src/components/ListItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <li class="todo">
+    <p>title</p>
+    <time class="expire">target date</time>
+    <div class="경고">
+      smi or bad
+    </div>
+  </li>
+</template>
+
+<style lang="scss" scoped>
+.todo {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  box-sizing: border-box;
+}
+</style>
