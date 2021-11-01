a44f8bdec04c24087c156bf773ffda98b27e04aa
iamidlek <iamidlek@naver.com>
2021-11-01 20:11:55 +0900

    드래그 앤 드롭을 컴포넌트와 연결하기 위한 몸부림

---
+++ drag&drop사용하기/DragTest2.vue
@@ -0,0 +1,17 @@
+<template>
+  <li>
+    <div>{{ element.name }}</div>
+    <div>{{ element.id }}</div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    element: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
