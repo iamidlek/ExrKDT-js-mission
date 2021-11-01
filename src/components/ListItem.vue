24802a6cba4379aab00ff6fe285e1b101ef36e04
iamidlek <iamidlek@naver.com>
2021-11-01 21:11:13 +0900

    done으로 이동 시키기 성공

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,40 +1,25 @@
 <template>
   <li class="todo">
     <transition name="icon">
-      <label
-        v-if="!change"
-        :for="item.id">
-        <input
-          :id="item.id"
-          type="checkbox" />
+      <label v-if="!change" :for="item.id">
+        <input :id="item.id" type="checkbox" @click="done" />
         <span>
           <i></i>
         </span>
       </label>
-      <div
-        v-else
-        class="del"
-        @click="del">
+      <div v-else class="del" @click="del">
         <div class="cross1"></div>
         <div class="cross2"></div>
       </div>
     </transition>
-    <div
-      class="title"
-      @click="viewTools">
-      <div>{{ item.title.split('__@dateSet-expire__Info:')[0] }}</div>
+    <div class="title" @click="viewTools">
+      <div>{{ item.title.split("__@dateSet-expire__Info:")[0] }}</div>
     </div>
     <transition name="icon">
-      <p
-        v-if="!change"
-        class="due"
-        :class="{expire : calcDate}">
-        {{ item.title.split('__@dateSet-expire__Info:')[1].slice(-5) }}
+      <p v-if="!change" class="due" :class="{ expire: calcDate }">
+        {{ item.title.split("__@dateSet-expire__Info:")[1].slice(-5) }}
       </p>
-      <div
-        v-else
-        class="fix"
-        @click="upCurrLiData">
+      <div v-else class="fix" @click="upCurrLiData">
         <span></span>
       </div>
     </transition>
@@ -42,68 +27,97 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   props: {
     item: {
       type: Object,
-      default: () => ({})
+      default: () => ({}),
     },
     user: {
       type: String,
-      default: ''
+      default: "",
+    },
+    donelen: {
+      type: Number,
+      default: 0,
     },
   },
-  emits: ['reGetList','fixInfo'],
+  emits: ["reGetList", "fixInfo", "moveToDone"],
   data() {
     return {
       change: false,
-    }
+    };
   },
   computed: {
     calcDate() {
-      const date = new Date()
-      const setted = new Date(this.item.title.split('__@dateSet-expire__Info:')[1])
+      const date = new Date();
+      const setted = new Date(
+        this.item.title.split("__@dateSet-expire__Info:")[1]
+      );
       if (date > setted) {
-        return true
+        return true;
       } else {
-        return false
+        return false;
       }
-    }
+    },
   },
   methods: {
     async deleteTodo() {
       await axios({
-      url: `https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos/${this.item.id}`,
-      method: 'delete',
-      headers: {
-        'content-type': 'application/json',
-        'apikey': 'FcKdtJs202110',
-        'username': this.user
-        }
-      })
+        url: `https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos/${this.item.id}`,
+        method: "delete",
+        headers: {
+          "content-type": "application/json",
+          apikey: "FcKdtJs202110",
+          username: this.user,
+        },
+      });
     },
     async del() {
-      await this.deleteTodo()
-      this.$emit('reGetList')
+      await this.deleteTodo();
+      this.$emit("reGetList");
     },
     upCurrLiData() {
       const liData = {
         id: this.item.id,
         order: this.item.order,
-        title: this.item.title
-      }
-      this.$emit('fixInfo', liData)
+        title: this.item.title,
+        modiDate: this.item.updatedAt,
+      };
+      this.$emit("fixInfo", liData);
     },
     viewTools() {
       if (this.change !== true) {
-        this.change = !this.change
-        setTimeout(()=> this.change = !this.change,3000)
+        this.change = !this.change;
+        setTimeout(() => (this.change = !this.change), 3000);
       }
-    }
-  }
-}
+    },
+    async doneTodo() {
+      await axios({
+        url: `https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos/${this.item.id}`,
+        method: "put",
+        headers: {
+          "content-type": "application/json",
+          apikey: "FcKdtJs202110",
+          username: this.user,
+        },
+        data: {
+          order: 3000 + this.donelen,
+          title: this.item.title,
+          done: true,
+        },
+      });
+    },
+    async done(e) {
+      if (e.target.checked) {
+        await this.doneTodo();
+        this.$emit("moveToDone");
+      }
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -127,7 +141,7 @@ export default {
     margin: 0;
     font-size: 1.1em;
     padding-left: 32px;
-    transition: .6s ease-in-out;
+    transition: 0.6s ease-in-out;
     cursor: pointer;
     & > div {
       width: 157px;
@@ -143,7 +157,7 @@ export default {
     font-size: 1.1em;
     pointer-events: none;
     &.expire {
-    color: #d80808;
+      color: #d80808;
     }
   }
   .fix {
@@ -157,18 +171,23 @@ export default {
     border: 2px solid rgba(255, 255, 255, 0.4);
     border-radius: 4px;
     &::before {
-      content: '';
+      content: "";
       position: absolute;
       border-bottom: 2px solid rgba(255, 255, 255, 0.4);
       border-left: 2px solid rgba(255, 255, 255, 0.4);
-      background: linear-gradient(45deg, rgba(255, 255, 255, 0.4) 50%, transparent 50%, transparent 100%);
+      background: linear-gradient(
+        45deg,
+        rgba(255, 255, 255, 0.4) 50%,
+        transparent 50%,
+        transparent 100%
+      );
       width: 3px;
       height: 3px;
       top: 4px;
       right: 3px;
     }
     &::after {
-      content: '';
+      content: "";
       position: absolute;
       border: 2px solid rgba(255, 255, 255, 0.4);
       border-left-color: transparent;
@@ -200,22 +219,23 @@ export default {
     .cross1 {
       width: 18px;
       height: 18px;
-      border-right: 2px solid rgba(#efefef, .5);
+      border-right: 2px solid rgba(#efefef, 0.5);
       transform: rotate(45deg) translateX(-2px);
     }
     .cross2 {
       width: 18px;
       height: 18px;
-      border-right: 2px solid rgba(#efefef, .5);
+      border-right: 2px solid rgba(#efefef, 0.5);
       transform: rotate(-45deg) translateX(4px) translateY(-6px);
     }
     &:hover {
-      .cross1, .cross2 {
-        border-color: rgba(#efefef, .9);
+      .cross1,
+      .cross2 {
+        border-color: rgba(#efefef, 0.9);
       }
     }
   }
-  label{
+  label {
     width: 28px;
     height: 28px;
     position: absolute;
@@ -228,10 +248,10 @@ export default {
       pointer-events: none;
       &:checked {
         & ~ span {
-          background-color: #05BE05;
-          box-shadow: 0 5px 5px #05BE0566;
-          i::after{
-            content: '';
+          background-color: #05be05;
+          box-shadow: 0 5px 5px #05be0566;
+          i::after {
+            content: "";
             bottom: 12px;
             height: 8px;
             border-radius: 0;
@@ -239,7 +259,7 @@ export default {
             border-bottom-right-radius: 15px;
           }
         }
-      } 
+      }
     }
     span {
       position: absolute;
@@ -250,16 +270,16 @@ export default {
       background-color: #d80808;
       border-radius: 80px;
       transition: 0.5s;
-      box-shadow: 0 5px 5px #FE000044;
-      pointer-events: none; 
-      i{
+      box-shadow: 0 5px 5px #fe000044;
+      pointer-events: none;
+      i {
         position: absolute;
         top: -16px;
         left: -6px;
         width: 57px;
         height: 52px;
         &::before {
-          content: '';
+          content: "";
           position: absolute;
           top: 24px;
           left: 12px;
@@ -270,8 +290,8 @@ export default {
           /* 오른쪽 눈 */
           box-shadow: 10px 0 0 #efefefce;
         }
-        &::after{
-          content: '';
+        &::after {
+          content: "";
           position: absolute;
           bottom: 16px;
           left: calc(50% - 15px);
@@ -285,11 +305,11 @@ export default {
     }
     &:hover {
       span {
-        background-color: #05BE05;
-        box-shadow: 0 5px 5px #05BE0566;
+        background-color: #05be05;
+        box-shadow: 0 5px 5px #05be0566;
       }
-      i::after{
-        content: '';
+      i::after {
+        content: "";
         bottom: 12px;
         height: 8px;
         border-radius: 0;
@@ -299,15 +319,15 @@ export default {
     }
   }
 }
-.icon-enter-active{
+.icon-enter-active {
   opacity: 1;
-  transition: opacity .25s linear;
+  transition: opacity 0.25s linear;
 }
 .icon-leave-active {
   opacity: 0;
-  transition: opacity .25s linear;
+  transition: opacity 0.25s linear;
 }
-.icon-enter-from{
+.icon-enter-from {
   opacity: 0;
 }
 </style>
