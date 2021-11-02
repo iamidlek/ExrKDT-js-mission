28ecc26327150791f74accaa8686dfd43232e122
iamidlek <iamidlek@naver.com>
2021-11-02 19:02:09 +0900

    ref

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,50 @@
 <template>
   <BackGround />
-  <transition
-    name="fade">
-    <CheckName
-      v-if="!admission"
-      v-model="open"
-      :user="userName"
-      @enroll="signIn" />
-    <ToDoView
-      v-else-if="open"
-      :user="userName" />
+  <transition name="fade">
+    <div v-if="effect"></div>
+    <CheckName v-else-if="!user" />
+    <ToDoView v-else />
   </transition>
 </template> 
 
 <script>
-import BackGround from '~/components/BackGround'
-import CheckName from '~/components/CheckName'
-import ToDoView from '~/components/ToDoView'
+import BackGround from "~/components/BackGround";
+import CheckName from "~/components/CheckName";
+import ToDoView from "~/components/ToDoView";
 
 export default {
   components: {
     BackGround,
     CheckName,
-    ToDoView
+    ToDoView,
   },
   data() {
     return {
-      userName: null,
-      admission: true,
-      open: false
-    }
+      effect: true,
+    };
+  },
+  computed: {
+    user() {
+      return this.$store.state.todo.user;
+    },
   },
   mounted() {
-    setTimeout(()=>{this.admission = false},100)
-    setTimeout(()=>{this.checkStorage()},2000)
+    setTimeout(() => (this.effect = false), 100);
+    this.$store.commit("todo/checkUser");
   },
-  methods: {
-    checkStorage() {
-      const user = localStorage.getItem('todo') 
-      if (user) {
-        this.admission = true
-        this.userName = user
-      }
-    },
-    signIn(res) {
-      this.admission = res
-      this.userName = localStorage.getItem('todo')
-    }
-  }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-.fade-enter-active{
+.fade-enter-active {
   opacity: 1;
-  transition: opacity 2s linear;
+  transition: 2s linear;
 }
 .fade-leave-active {
   opacity: 0;
-  transition: opacity 1s linear;
+  transition: 1s linear;
 }
-.fade-enter-from{
+.fade-enter-from {
   opacity: 0;
 }
 </style>
