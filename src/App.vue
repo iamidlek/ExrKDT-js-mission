ab2ae30db5ef2ddc225d1681323274a373d85b2f
iamidlek <iamidlek@naver.com>
2021-11-08 01:05:32 +0900

    axios test 완료

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
   </div>
 </template>
 <script>
+// import axios from 'axios'
+// import { createItem } from '~/api/axiosPost'
+import { getTree } from '~/api/axiosGet'
 
 export default {
   components: {
@@ -12,7 +15,12 @@ export default {
     return {
     } 
   },
-  created() {
+  async created() {
+    console.log(await getTree())
+    // await createItem('3FoGSg1ZXwdcX2XhERDW', '자식 1뎁스', '내용도 그냥저냥')
   },
+  mounted() {
+  //   setTimeout(()=>{console.log(data)},2000)
+  }
 }
 </script>
