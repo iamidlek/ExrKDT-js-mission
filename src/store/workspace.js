133e0614652f00b49e7d1d76d83fd7520c54ea7b
iamidlek <iamidlek@naver.com>
2021-11-08 01:05:59 +0900

    store 설정해보기

--- src/store/workspace.js
+++ src/store/workspace.js
@@ -1,14 +1,25 @@
 // import axios from 'axios'
+import { getTree } from '~/api/axiosGet'
 
 export default {
   namespaced: true,
   state: () => ({
-    
+    userName: 'YooHyungChul',
+    workspaceTree: []
   }),
   getters: {
   },
   mutations: {
+    assignState(state, payload) {
+      Object.keys(payload).forEach(key => {
+        state[key] = payload[key]
+      })
+    }
   },
   actions: {
+    // async getWorkspaceTree () {
+    //   const tree = await getTree()
+
+    // }
   }
 }
