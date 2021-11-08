f8805f6e1ed7ce41e2b7a52ff23398aa129aa2d2
iamidlek <iamidlek@naver.com>
2021-11-09 03:49:16 +0900

    conf App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
-  <div>
-    list 
+  <div class="wrapper">
+    <div class="bar">
+      <WorkSpaceBar />
+      <div class="handle"></div>
+    </div>
+    <div class="size-controler"></div>
+    <div class="main-content">
+      <WorkSpaceContent />
+    </div>
   </div>
 </template>
 
 <script>
+import WorkSpaceBar from '~/components/WorkSpaceBar'
+import WorkSpaceContent from '~/components/WorkSpaceContent'
+
 export default {
   components: {
+    WorkSpaceBar,
+    WorkSpaceContent
   },
   data() {
     return {
@@ -17,9 +29,43 @@ export default {
   },
   async created() {
     await this.$store.dispatch('workspace/getWorkspaceTree')
-    console.log(this.$store.state.workspace.workspaceTree)
   },
   mounted() {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  height: 100vh;
+  color: #e8e7e4;
+  .bar {
+    position: relative;
+    box-sizing: border-box;
+    background-color: #373c3f;
+    max-width: 480px;
+    width: 320px;
+    // min-width: 190px;
+    padding-left: 1rem;
+    display: flex;
+    flex-direction: column;
+    .handle {
+      position: absolute;
+      top: 0;
+      right: -2px;
+      width: 6px;
+      height: 100%;
+      opacity: .7;
+      &:hover {
+        background-color: yellowgreen;
+
+      }
+    }
+  }
+  .main-content {
+    flex: 1;
+    background-color: #2f3437;
+  }
+}
+</style>
