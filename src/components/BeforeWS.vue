c25d8f46b571547d2c040d68ccec7f974b7b163b
iamidlek <iamidlek@naver.com>
2021-11-09 03:49:56 +0900

    create main

---
+++ src/components/BeforeWS.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="guide">
+    새로운 작업을 시작해 주세요
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.guide {
+  margin-top: 20%;
+  text-align: center;
+  font-size: 3rem;
+}
+</style>
