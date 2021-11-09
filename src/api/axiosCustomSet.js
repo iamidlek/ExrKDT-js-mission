ee820d160e75d16c7a69fb528bd5f51766da9ae8
iamidlek <iamidlek@naver.com>
2021-11-09 11:38:43 +0900

    apikey 문제인지 확인

--- src/api/axiosCustomSet.js
+++ src/api/axiosCustomSet.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
-const { apikey } = process.env
+// const { apikey } = process.env
 
 const axiosCustomSet = axios.create({
   baseURL: 'https://asia-northeast3-heropy-api.cloudfunctions.net/api/notion/workspaces',
   headers: {
     'content-type': 'application/json',
-    apikey,
+    apikey: 'FcKdtJs202110',
     username: 'YooHyungChul'
   },
 })
